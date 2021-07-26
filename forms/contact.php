0a65d7af9afd6e3c7115e699ca8fa73ca684a50f
Pablo Garay <pgaray@finnegans.com.ar>
2021-07-26 18:24:08 -0300

    pruebita

--- forms/contact.php
+++ forms/contact.php
@@ -21,17 +21,17 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.hostinger.com',
+    'username' => 'formulario@impulsatuidea.com',
+    'password' => 'oKtubre86',
+    'port' => '465'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
